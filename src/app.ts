467b43b66f2051714907401c46944b5a0c3f1ee9
Daniil Raptanov <daniilraptanov@gmail.com>
2022-08-15 11:29:30 +0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,26 @@
 import express from "express";
-import { swaggerOptions } from "../swagger-settings";
 import { DBCreator } from "./db/database";
 import { AuthValidator } from "./middleware/validators/auth-validator";
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerJSDoc = require("swagger-jsdoc");
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "../swagger-doc";
 
 const cors = require("cors");
 require('dotenv-safe').config();
 
 const port = process.env.PORT;
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/swagger.json', (_req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-});
+// app.get('/swagger.json', (_req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(swaggerSpec);
+// });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use("/api/v1", require("./routes/auth-routes"));
 app.use("/api/v1", AuthValidator.auth, require("./routes/template-routes"));
 
---
+++ swagger-doc.ts
@@ -0,0 +1,150 @@
+export default {
+  openapi: "3.0.0",
+  info: {
+    title: "TDCH-MAIN-API",
+    version: 1,
+    description: "Auth & main API description for tdch-main-client",
+  },
+  servers: [
+    {
+      url: "https://tdch-main-api.herokuapp.com/",
+      description: "DEV Env",
+    },
+  ],
+  tags: [
+    {
+      name: "Auth",
+      description: "Auth endpoints for login/registration user account",
+    },
+  ],
+  components: {
+    schemas: {
+      User: {
+        type: "object",
+        properties: {
+          id: {
+            type: "string",
+            description: "User unique id",
+            example: "a7f55de1-cee6-49a3-9d07-3aa47a045c11",
+          },
+          email: {
+            type: "string",
+            description: "User email",
+            example: "user@gmail.com",
+          },
+          weeks: {
+            type: "array",
+            description: "Personal User array of Week",
+            $ref: "#/components/schemas/Week",
+          },
+        },
+      },
+
+      Week: {
+        type: "object",
+        properties: {
+          id: {
+            type: "string",
+            description: "Week unique id",
+            example: "a7f55de1-cee6-49a3-9d07-3aa47a045c11",
+          },
+          templateId: {
+            type: "string",
+            description: "Template id",
+            example: "a7f55de1-cee6-49a3-9d07-3aa47a045c11",
+          },
+          tasks: {
+            type: "array",
+            description: "Week array of tasks",
+            $ref: "#/components/schemas/Task",
+          },
+        },
+      },
+
+      Task: {
+        type: "object",
+        properties: {
+          id: {
+            type: "string",
+            description: "Task unique id",
+            example: "a7f55de1-cee6-49a3-9d07-3aa47a045c11",
+          },
+          name: {
+            type: "string",
+            description: "Task name",
+            example: "programming",
+          },
+          time: {
+            type: "string",
+            description: "Task time (in format <hh:mm>)",
+            example: "09:30",
+          },
+          day: {
+            type: "number",
+            description: "Task day (number 1-7)",
+            example: "5",
+          },
+          status: {
+            type: "number",
+            description: "Task status (number 1-3)",
+            example: "2",
+          },
+        },
+      },
+    },
+  },
+  paths: { // TODO :: add /auth/register
+    "/auth/login": {
+      post: {
+        tags: ["Auth"],
+        summary: "User login",
+        description: "User login to his account",
+        requestBody: {
+          content: {
+            "application/json": {
+              properties: {
+                email: {
+                  type: "string",
+                  description: "User email",
+                  example: "user@gmail.com",
+                },
+                password: {
+                  type: "string",
+                  description: "User password",
+                  example: "very-secret-password",
+                },
+              },
+            },
+          },
+          required: true,
+        },
+        responses: { // TODO :: add correctly responses data
+          "200": {
+            description: "Successful operation",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/User",
+                },
+              }
+            },
+          },
+          "400": {
+            description: "Invalid ID supplied",
+          },
+          "404": {
+            description: "Pet not found",
+          },
+          "405": {
+            description: "Validation exception",
+          },
+        },
+        security: [
+          {
+            petstore_auth: ["write:pets", "read:pets"],
+          },
+        ],
+      },
+    },
+  },
+};
