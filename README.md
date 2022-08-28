b6bb37e48e5c87270488506fbfefd2b8ca4c367f
Daniil Raptanov <daniilraptanov@gmail.com>
2022-08-28 18:04:11 +0300

    db settings

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ﻿# tdch-auth-api
+
+1) create ".env" file
+2) start command "npm install"
+3) start command "npm run server"
--- src/db/database.ts
+++ src/db/database.ts
@@ -3,16 +3,7 @@ require('dotenv-safe').config();
 
 
 export class DBConnector {
-    private static _db: Sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-          dialect: "postgres",
-          host: process.env.DB_HOST,
-          port: Number(process.env.DB_PORT)
-        }
-    ); 
+    private static _db: Sequelize = new Sequelize(process.env.DATABASE_URI); 
 
     private constructor() {}
 
